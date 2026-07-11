95ec1a03974b25f8956f6d2b784c1c9f04b35604
pranayjhanwar01-dev <pranayjhanwar01@gmail.com>
2026-07-12 00:33:43 +0530

    DAY 9

---
+++ Day 9/register.php
@@ -0,0 +1,22 @@
+<?php
+
+include("db_connect.php");
+if(isset($_POST["btn_submit"])){
+$name = $_POST["user_name"];
+$email = $_POST["user_email"];
+$branch = $_POST["user_branch"];
+$cgpa = $_POST["user_cgpa"];
+$phone = $_POST["user_mobile"];
+$city = $_POST["user_city"];
+
+$sql = "INSERT INTO STUDENT( NAME , EMAIL , BRANCH , CGPA , PHONE , CITY ) values('$name' , '$email' , '$branch' , '$cgpa' , '$phone' , '$city')";
+$result = mysqli_query($conn , $sql);
+
+if($result){
+    echo "you are successfully registered";
+}else{
+    echo "there is some error plese try again ";
+}
+}
+
+?>
